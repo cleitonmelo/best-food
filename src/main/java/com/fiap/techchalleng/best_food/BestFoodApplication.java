45dbb34ea991f77df439059c0f02ce8e92bdd811
Cleiton Melo Da Silva <cleiton.silva@gmmti.com.br>
2024-09-06 16:29:03 -0300

    Adicionando swagger

--- src/main/java/com/fiap/techchalleng/best_food/BestFoodApplication.java
+++ src/main/java/com/fiap/techchalleng/best_food/BestFoodApplication.java
@@ -1,9 +1,18 @@
 package com.fiap.techchalleng.best_food;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Best Food Application",
+				description = "Tech Challeng 3 - FIAP 2024 - Grupo 18",
+				version = "1.0.0"
+		)
+)
 public class BestFoodApplication {
 
 	public static void main(String[] args) {
