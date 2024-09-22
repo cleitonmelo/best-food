f24bf801081a0c6f6ed16653fb9acbab4a3247f0
Vinicius Quadros <voa.quadros@gmail.com>
2024-09-22 12:29:14 -0300

    implementação do buscaRestaurante

--- src/main/java/com/fiap/techchalleng/best_food/domain/gateway/restaurante/RestauranteInterface.java
+++ src/main/java/com/fiap/techchalleng/best_food/domain/gateway/restaurante/RestauranteInterface.java
@@ -2,15 +2,22 @@ package com.fiap.techchalleng.best_food.domain.gateway.restaurante;
 
 import com.fiap.techchalleng.best_food.domain.entity.restaurante.Mesa;
 import com.fiap.techchalleng.best_food.domain.entity.restaurante.Restaurante;
+import com.fiap.techchalleng.best_food.domain.enums.restaurante.TipoCozinha;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface RestauranteInterface {
 
-    List<Restaurante> buscaRestaurantes();
+    List<Restaurante> getRestaurantes();
 
     Restaurante getRestauranteById(UUID id);
+
+    List<Restaurante> getRestaurantesByName(String nome);
+
+    List<Restaurante> getRestaurantesByBairro(String bairro);
+
+    List<Restaurante> getRestaurantesByTipoCozinha(TipoCozinha tipoCozinha);
 
     Restaurante createRestaurante(Restaurante restaurante, List<Mesa> mesas);
 }
